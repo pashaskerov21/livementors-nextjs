5dc76454762c840cfb5c08a87c4eb4194a1c62e8
pasha <askerovpasha21@gmail.com>
2024-04-18 12:22:14 +0400

    next config

--- next.config.js
+++ next.config.js
@@ -10,7 +10,8 @@ const nextConfig = {
         ],
     },
     env: {
-        API_URL: process.env.API_URL
+        API_URL: process.env.API_URL,
+        SITE_URL: process.env.SITE_URL,
     },
 }
 
